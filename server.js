0a4bc130f7d4a01eae5b2b56665f76827de2a5f9
josephjasper648 <josephjasper05@gmail.com>
2025-09-15 05:33:39 +0530

    add xtra files

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+ res.json({message:"Hello from the server!"});
+})
+
+app.listen(3001,'localhost', () => {
+    console.log(`Server is running on http://localhost:3001`);
+});
+
