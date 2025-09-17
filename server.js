0cbe8a6983e8262433235c1956b53186bbdee082
josephjasper648 <josephjasper05@gmail.com>
2025-09-17 16:36:54 +0530

    add mongodb and env

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const app = express();
+require('dotenv').config();
 
 app.get('/',(req,res)=>{
- res.json({message:"Hello from the server!"});
+ res.json({message:"Hello from the nodemon server!"});
 })
 
 app.listen(3001,'localhost', () => {
     console.log(`Server is running on http://localhost:3001`);
 });
 
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(()=>console.log("connected to mongoDB"))
+    .catch((err)=>console.error('could not connect to mongoDB',err));
